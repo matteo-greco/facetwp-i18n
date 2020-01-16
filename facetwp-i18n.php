ac5a6832c2b13c8b8eecd54c2192ea7a0eaa57fd
Matteo Greco <matteo@matteogreco.net>
2020-01-16 18:15:17 +0100

    Initial test version

--- facetwp-i18n.php
+++ facetwp-i18n.php
@@ -2,36 +2,45 @@
 /*
 Plugin Name: FacetWP - Multilingual support
 Description: Multilingual support for FacetWP
-Version: 0.2.1
+Version: 0.2.2
 Author: FacetWP, LLC
 Author URI: https://facetwp.com/
 GitHub URI: facetwp/facetwp-i18n
 */
 
-defined( 'ABSPATH' ) or exit;
+defined( 'ABSPATH' ) || exit;
 
-class FWP_i18n
-{
+/**
+ * Class FWP_i18n
+ */
+class FWP_i18n {
 
-    function __construct() {
-        add_action( 'init' , array( $this, 'init' ) );
-    }
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		add_action( 'init', array( $this, 'init' ) );
+	}
 
 
-    /**
-     * Intialize
-     */
-    function init() {
-        if ( function_exists( 'FWP' ) ) {
-            if ( function_exists( 'pll_register_string' ) ) {
-                include( dirname( __FILE__ ) . '/includes/class-polylang.php' );
-            }
+	/**
+	 * Intialize
+	 */
+	public function init() {
+		if ( function_exists( 'FWP' ) ) {
+			if ( function_exists( 'pll_register_string' ) ) {
+				include dirname( __FILE__ ) . '/includes/class-polylang.php';
+			}
 
-            if ( defined( 'ICL_SITEPRESS_VERSION' ) ) {
-                include( dirname( __FILE__ ) . '/includes/class-wpml.php' );
-            }
-        }
-    }
+			if ( defined( 'ICL_SITEPRESS_VERSION' ) ) {
+				include dirname( __FILE__ ) . '/includes/class-wpml.php';
+			}
+
+			if ( function_exists( 'trp_enable_translatepress' ) ) {
+				include dirname( __FILE__ ) . '/includes/class-translatepress.php';
+			}
+		}
+	}
 }
 
 new FWP_i18n();
